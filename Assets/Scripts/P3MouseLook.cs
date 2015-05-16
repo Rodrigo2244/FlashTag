0dbd8a845724ac37077905da85b4d4e52a67bd4e
valdeezzee <jvalderrama91@hotmail.com>
2015-05-16 13:20:21 -0400

    Character controllers

--- Assets/Scripts/P3MouseLook.cs
+++ Assets/Scripts/P3MouseLook.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 /// MouseLook rotates the transform based on the mouse delta.
@@ -15,39 +15,39 @@ using System.Collections;
 /// - Add a MouseLook script to the camera.
 ///   -> Set the mouse look to use LookY. (You want the camera to tilt up and down like a head. The character already turns.)
 [AddComponentMenu("Camera-Control/Mouse Look")]
-public class MouseLook : MonoBehaviour {
-
+public class P3MouseLook : MonoBehaviour {
+	
 	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
 	public RotationAxes axes = RotationAxes.MouseXAndY;
 	public float sensitivityX = 15F;
 	public float sensitivityY = 15F;
-
+	
 	public float minimumX = -360F;
 	public float maximumX = 360F;
-
+	
 	public float minimumY = -60F;
 	public float maximumY = 60F;
-
+	
 	float rotationY = 0F;
-
+	
 	void Update ()
 	{
 		if (axes == RotationAxes.MouseXAndY)
 		{
-			float rotationX = transform.localEulerAngles.y + Input.GetAxis("P1Mouse X") * sensitivityX;
+			float rotationX = transform.localEulerAngles.y + Input.GetAxis("P3Mouse X") * sensitivityX;
 			
-			rotationY += Input.GetAxis("P1Mouse Y") * sensitivityY;
+			rotationY += Input.GetAxis("P3Mouse Y") * sensitivityY;
 			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
 			
 			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
 		}
 		else if (axes == RotationAxes.MouseX)
 		{
-			transform.Rotate(0, Input.GetAxis("P1Mouse X") * sensitivityX, 0);
+			transform.Rotate(0, Input.GetAxis("P3Mouse X") * sensitivityX, 0);
 		}
 		else
 		{
-			rotationY += Input.GetAxis("P1Mouse Y") * sensitivityY;
+			rotationY += Input.GetAxis("P3Mouse Y") * sensitivityY;
 			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
 			
 			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
--- Assets/Scripts/Player2InputController.js
+++ Assets/Scripts/Player2InputController.js
@@ -1,4 +1,4 @@
-private var motor : CharacterMotor;
+﻿private var motor : CharacterMotor;
 
 // Use this for initialization
 function Awake () {
@@ -8,7 +8,7 @@ function Awake () {
 // Update is called once per frame
 function Update () {
 	// Get the input vector from keyboard or analog stick
-	var directionVector = new Vector3(Input.GetAxis("P1Horizontal"), 0, Input.GetAxis("P1Vertical"));
+	var directionVector = new Vector3(Input.GetAxis("P2Horizontal"), 0, Input.GetAxis("P2Vertical"));
 	
 	if (directionVector != Vector3.zero) {
 		// Get the length of the directon vector and then normalize it
@@ -29,7 +29,7 @@ function Update () {
 	
 	// Apply the direction to the CharacterMotor
 	motor.inputMoveDirection = transform.rotation * directionVector;
-	motor.inputJump = Input.GetButton("Jump1");
+	motor.inputJump = Input.GetButton("Jump2");
 }
 
 // Require a character controller to be attached to the same game object
