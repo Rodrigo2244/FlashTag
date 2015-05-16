8596bb23270e88e6870c8b8674a197dc8eff6dd9
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2015-05-16 14:44:03 -0400

    LightObjects diminish

--- Assets/Scripts/scriptLightObject.cs
+++ Assets/Scripts/scriptLightObject.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class scriptLightObject : MonoBehaviour {
-
+	
 	void OnCollisionEnter(Collision col){
 		if(col.gameObject.tag == "Respawn"){
 			Debug.Log("hit");
@@ -10,6 +10,14 @@ public class scriptLightObject : MonoBehaviour {
 			light.color = col.gameObject.light.color;
 			renderer.enabled = true;
 			renderer.material.color = col.gameObject.light.color;
+			StartCoroutine(Diminish());
 		}
+	}
+
+	IEnumerator Diminish(){
+		animation.Play("LightObject");
+		yield return new WaitForSeconds(10f);
+		light.enabled = false;
+		renderer.enabled = false;
 	}
 }
