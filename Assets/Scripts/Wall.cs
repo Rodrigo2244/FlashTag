5c544a4c5a1582c21e53ad5cd340214ab2dfccff
C-Through <drew.l.connell@gmail.com>
2015-05-16 11:50:26 -0400

    Basic emissive and wall script
    Generic texture, and materials folder

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour 
+{
+    private MeshRenderer meshRenderer;
+
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        ChangeColor(other.gameObject);
+    }
+
+	private void ChangeColor(GameObject gameObject)
+    {
+        if (gameObject.GetComponent<scriptLightBullet>() == null)
+            return;
+
+        meshRenderer.enabled = true;
+
+        Light light = gameObject.GetComponent<Light>();
+
+        meshRenderer.material.color = light.color;
+    }
+}
