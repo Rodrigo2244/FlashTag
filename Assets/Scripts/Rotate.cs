01f6e4aaa1279262077da3ff3c4c9d9a413e48af
C-Through <drew.l.connell@gmail.com>
2015-05-16 15:36:07 -0400

    New prefab

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour 
+{
+    private float rate = Time.deltaTime * 10.0f;
+
+	void Update () 
+    {
+        transform.Rotate(Vector3.right * rate);
+	}
+}
