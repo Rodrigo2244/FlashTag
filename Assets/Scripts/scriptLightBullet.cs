a65cb9a1d29a71f9a9246fe4cf8d976620d01999
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2015-05-16 00:42:54 -0400

    Created Repo

---
+++ Assets/Scripts/scriptLightBullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptLightBullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddForce(transform.forward*500);
+		StartCoroutine(Die ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator Die(){
+		yield return new WaitForSeconds(10);
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/scriptShootLight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptShootLight : MonoBehaviour {
+
+	Transform camera;
+	public GameObject lightBullet;
+
+	// Use this for initialization
+	void Start () {
+		camera = transform.GetChild(1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine(camera.position, camera.position + camera.forward,Color.red);
+
+		if(Input.GetMouseButtonDown(0)){
+			Instantiate(lightBullet,camera.forward,camera.rotation);
+		}
+	}
+}
