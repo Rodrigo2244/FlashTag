3cb7788d992196977e7550d4d7919922e2166083
valdeezzee <jvalderrama91@hotmail.com>
2015-05-16 15:22:39 -0400

    Working on game manager

--- Assets/Scripts/scriptGameManager.cs
+++ Assets/Scripts/scriptGameManager.cs
@@ -4,14 +4,38 @@ using System.Collections;
 public class scriptGameManager : MonoBehaviour {
 
 	public int playerNum = 2;
+	private bool setPlayers;
+	private GameObject playerActivate;
+	private GameObject playerCam;
+	private Camera playerCamSet;
 
 	void Start(){
 		DontDestroyOnLoad(this.gameObject);
 	}
 
 	void OnLevelWasLoaded(int level){
-		if(level == 1){
-			//Write code here
+		if(level == 1 && setPlayers == false){
+			if(playerNum == 2)
+			{	print ("YOOO");
+				playerCam = GameObject.Find("P1Cam");
+				playerCamSet = playerCam.GetComponent<Camera>();
+				playerCamSet.rect = new Rect(0, 0, 0.5f, 0.5f);
+
+				playerCam = GameObject.Find("P2Cam");
+				playerCamSet = playerCam.GetComponent<Camera>();
+				playerCamSet.rect = new Rect(0.5f , 0.5f, 0.5f, 1);
+			}
+			else if(playerNum == 3)
+			{
+
+
+			}
+			else if(playerNum == 4)
+			{
+
+
+			}
+
 		}
 	}
 }
