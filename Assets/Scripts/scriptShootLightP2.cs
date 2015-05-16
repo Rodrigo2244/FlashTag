78320fbe6d45c5c8f63bccfa21267855c95fc5df
valdeezzee <jvalderrama91@hotmail.com>
2015-05-16 13:56:33 -0400

    Character movement
    Mouse look. firing done

--- Assets/Scripts/scriptShootLightP2.cs
+++ Assets/Scripts/scriptShootLightP2.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class scriptShootLight : MonoBehaviour {
-
+public class scriptShootLightP2 : MonoBehaviour {
+	
 	Transform camera;
 	public GameObject lightBullet;
 	private bool oneShot = false;
-
+	
 	// Use this for initialization
 	void Start () {
 		camera = transform.GetChild(1);
@@ -15,14 +15,14 @@ public class scriptShootLight : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Debug.DrawLine(camera.position, camera.position + camera.forward,Color.red);
-		if(Input.GetAxisRaw("Fire1") == 0.0f)
+		if(Input.GetAxisRaw("Fire2") == 0.0f)
 		{
 			oneShot = false;
 		}
-		if(Input.GetAxisRaw("Fire1") == 0.1f && oneShot == false){
+		if(Input.GetAxisRaw("Fire2") == 0.1f && oneShot == false){
 			Instantiate(lightBullet,camera.position + camera.forward,camera.rotation);
 			oneShot = true;
-
+			
 		}
 	}
 }
--- Assets/Scripts/scriptShootLightP4.cs
+++ Assets/Scripts/scriptShootLightP4.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class scriptShootLight : MonoBehaviour {
-
+public class scriptShootLightP4 : MonoBehaviour {
+	
 	Transform camera;
 	public GameObject lightBullet;
 	private bool oneShot = false;
-
+	
 	// Use this for initialization
 	void Start () {
 		camera = transform.GetChild(1);
@@ -15,14 +15,15 @@ public class scriptShootLight : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Debug.DrawLine(camera.position, camera.position + camera.forward,Color.red);
-		if(Input.GetAxisRaw("Fire1") == 0.0f)
+		if(Input.GetAxisRaw("Fire4") == 0.0f)
 		{
 			oneShot = false;
 		}
-		if(Input.GetAxisRaw("Fire1") == 0.1f && oneShot == false){
+		if(Input.GetAxisRaw("Fire4") == 0.1f && oneShot == false){
 			Instantiate(lightBullet,camera.position + camera.forward,camera.rotation);
 			oneShot = true;
-
+			
 		}
 	}
 }
+
