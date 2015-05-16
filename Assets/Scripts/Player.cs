ed788b252e4113fe1f2c889507452d102f79edd8
C-Through <drew.l.connell@gmail.com>
2015-05-16 18:23:35 -0400

    Updated players

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,49 @@ public class Player : MonoBehaviour
 {
     public int ammo;
     public int health;
+    public int damage;
+    public int lives;
 
-    void OnTriggerEnter(Collider other)
+    private Vector3 origin;
+
+    void Awake()
     {
-        
+        health = 100;
+        damage = 50;
+        lives = 2;
+
+        origin = transform.position;
     }
 
+    void OnCollisionEnter(Collision other)
+    {
+        TestForDamage(other.gameObject);
+    }
+
+    private void TestForDamage(GameObject gameObject)
+    {
+        if(gameObject.tag != "Respawn")
+            return;
+
+        health -= damage;
+
+        // Test for death
+        if (health <= 0)
+            StartCoroutine("ResetPlayer");
+    }
+
+    private IEnumerator ResetPlayer()
+    {
+        //yield return new WaitForSeconds(5.0f);
+
+        lives--;
+
+        if (lives == 0)
+            Destroy(gameObject);
+
+        health = 100;
+        transform.position = origin;
+
+        yield return null;
+    }
 }
