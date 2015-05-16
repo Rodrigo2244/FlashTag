cda628f28ecbdb2b63c7f415a434257ac6b0a085
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2015-05-16 12:13:27 -0400

    Main Menu done

---
+++ Assets/Scripts/scriptGameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptGameManager : MonoBehaviour {
+
+	public int playerNum = 2;
+
+	void Start(){
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/scriptMainMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class scriptMainMenu : MonoBehaviour {
+
+	Text playerNumDisplay;
+	scriptGameManager gameManager;
+
+	void Start(){
+		playerNumDisplay = GameObject.Find("Player Num").transform.GetChild(0).GetComponent<Text>();
+		gameManager = GameObject.Find("_Game Manager").GetComponent<scriptGameManager>();
+	}
+
+	void Update(){
+		playerNumDisplay.text = gameManager.playerNum.ToString();
+	}
+
+	void Play(){
+		//Application.LoadLevel();
+	}
+
+	void Exit(){
+		Application.Quit();
+	}
+
+	void ChangePlayerNum(){
+		if(gameManager.playerNum != 4){
+			gameManager.playerNum++;
+		} else {
+			gameManager.playerNum = 2;
+		}
+	}
+}
