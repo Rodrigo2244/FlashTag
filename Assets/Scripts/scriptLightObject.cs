0c5061a26158c48935f508646e7605c610568e7a
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2015-05-16 14:03:34 -0400

    changed lightobject

---
+++ Assets/Scripts/scriptLightObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptLightObject : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.name == "Light Bullet"){
+			Debug.Log("hit");
+			light.enabled = true;
+			renderer.enabled = true;
+		}
+	}
+}
