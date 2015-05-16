67a11ed6335c8b8caf9f5036eab9b7ca64c57611
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2015-05-16 14:23:22 -0400

    changes to light

--- Assets/Scripts/scriptLightObject.cs
+++ Assets/Scripts/scriptLightObject.cs
@@ -7,7 +7,9 @@ public class scriptLightObject : MonoBehaviour {
 		if(col.gameObject.tag == "Respawn"){
 			Debug.Log("hit");
 			light.enabled = true;
+			light.color = col.gameObject.light.color;
 			renderer.enabled = true;
+			renderer.material.color = col.gameObject.light.color;
 		}
 	}
 }
