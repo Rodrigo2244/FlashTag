075130496d7522eab56b51b87477d617d3b97317
C-Through <drew.l.connell@gmail.com>
2015-05-16 16:46:37 -0400

    Started health

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+    public int ammo;
+    public int health;
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+}
