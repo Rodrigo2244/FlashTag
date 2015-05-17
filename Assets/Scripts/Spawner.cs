7ecaf3a0def68f82f11980115c40b851fec921b9
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2015-05-16 23:55:24 -0400

    Upside down quadrants

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,18 +4,20 @@ using System.Collections;
 public class Spawner : MonoBehaviour{
 	public GameObject[] layouts;
 	public Quaternion[] rotations;
+	public Quaternion[] otherRotations;
 
 	void Start(){
 		rotations = new Quaternion[4]{new Quaternion(0,0,0,0),new Quaternion(0,90,0,0),new Quaternion(0,180,0,0),new Quaternion(0,270,0,0)};
+		otherRotations = new Quaternion[1]{new Quaternion(180,0,0,0)};
 
 		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,0,0),rotations[Random.Range(0,rotations.Length)]);
 		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,0,24),rotations[Random.Range(0,rotations.Length)]);
 		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,0,0),rotations[Random.Range(0,rotations.Length)]);
 		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,0,24),rotations[Random.Range(0,rotations.Length)]);
 
-		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,8.44f,0),new Quaternion(180,rotations[Random.Range(0,rotations.Length)].y,0,0));
-		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,8.44f,24),new Quaternion(180,rotations[Random.Range(0,rotations.Length)].y,0,0));
-		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,8.44f,0),new Quaternion(180,rotations[Random.Range(0,rotations.Length)].y,0,0));
-		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,8.44f,24),new Quaternion(180,rotations[Random.Range(0,rotations.Length)].y,0,0));
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,8.44f,0),otherRotations[Random.Range(0,otherRotations.Length)]);
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,8.44f,24),otherRotations[Random.Range(0,otherRotations.Length)]);
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,8.44f,0),otherRotations[Random.Range(0,otherRotations.Length)]);
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,8.44f,24),otherRotations[Random.Range(0,otherRotations.Length)]);
 	}
 }
