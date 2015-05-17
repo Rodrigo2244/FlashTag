35878b411e5923c1313bb8e0d0e1767de1eb2497
valdeezzee <jvalderrama91@hotmail.com>
2015-05-16 20:23:39 -0400

    Shot delay

--- Assets/Scripts/scriptShootLightP3.cs
+++ Assets/Scripts/scriptShootLightP3.cs
@@ -7,6 +7,7 @@ public class scriptShootLightP3 : MonoBehaviour {
 	public GameObject lightBullet;
 	private bool oneShot = false;
 	public AudioClip shoot;
+	private float shotTimer = 0.0f;
 
 	void Start () {
 		camera = transform.GetChild(1);
@@ -15,15 +16,18 @@ public class scriptShootLightP3 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Debug.DrawLine(camera.position, camera.position + camera.forward,Color.red);
-		if(Input.GetAxisRaw("Fire3") == 0.0f)
-		{
-			oneShot = false;
-		}
-		if(Input.GetAxisRaw("Fire3") == 0.1f && oneShot == false){
+
+		if(Input.GetAxisRaw("Fire3") == 0.1f && shotTimer == 0){
 			transform.GetChild(1).GetChild(0).gameObject.animation.Play();
 			Instantiate(lightBullet,camera.position + camera.forward,camera.rotation);
 			oneShot = true;
 			AudioSource.PlayClipAtPoint(shoot,transform.position);
+			shotTimer = 1.0f;
+		}
+		shotTimer -= Time.deltaTime;
+		if(shotTimer < 0.0f)
+		{
+			shotTimer = 0.0f;
 		}
 	}
 }
