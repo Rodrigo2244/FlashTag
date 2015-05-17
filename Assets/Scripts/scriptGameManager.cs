dda01766d542114eea1fa6db83284850cebfbc35
valdeezzee <jvalderrama91@hotmail.com>
2015-05-17 11:04:05 -0400

    End game

--- Assets/Scripts/scriptGameManager.cs
+++ Assets/Scripts/scriptGameManager.cs
@@ -8,13 +8,11 @@ public class scriptGameManager : MonoBehaviour {
 	private GameObject playerActivate;
 	private GameObject playerCam;
 	private Camera playerCamSet;
-	private Vector3 player1Spawn;
-	private Vector3 player2Spawn;
-	private Vector3 player3Spawn;
-	private Vector3 player4Spawn;
+	private GameObject[] players;
 	public int deadPlayers = 0;
 	public bool stalactites;
 	public bool invisibility;
+
 
 	void Start(){
 		DontDestroyOnLoad(this.gameObject);
@@ -25,13 +23,13 @@ public class scriptGameManager : MonoBehaviour {
 			if(playerNum == 2)
 			{	//P1
 				playerActivate = GameObject.Find("Player1");
-				player1Spawn = playerActivate.transform.position;
+
 				playerCam = GameObject.Find("P1Cam");
 				playerCamSet = playerCam.GetComponent<Camera>();
 				playerCamSet.rect = new Rect(0, 0.5f, 1, 0.5f);
 				//P2
 				playerActivate = GameObject.Find("Player2");
-				player2Spawn = playerActivate.transform.position;
+
 				playerCam = GameObject.Find("P2Cam");
 				playerCamSet = playerCam.GetComponent<Camera>();
 				playerCamSet.rect = new Rect(0 , 0, 1, 0.5f);
@@ -45,19 +43,19 @@ public class scriptGameManager : MonoBehaviour {
 			else if(playerNum == 3)
 			{
 				playerActivate = GameObject.Find("Player1");
-				player1Spawn = playerActivate.transform.position;
+
 				playerCam = GameObject.Find("P1Cam");
 				playerCamSet = playerCam.GetComponent<Camera>();
 				playerCamSet.rect = new Rect(0, 0.5f, 0.5f, 0.5f);
 
 				playerActivate = GameObject.Find("Player2");
-				player2Spawn = playerActivate.transform.position;
+
 				playerCam = GameObject.Find("P2Cam");
 				playerCamSet = playerCam.GetComponent<Camera>();
 				playerCamSet.rect = new Rect(0.5f , 0.5f, 0.5f, 0.5f);
 
 				playerActivate = GameObject.Find("Player3");
-				player3Spawn = playerActivate.transform.position;
+
 				playerCam = GameObject.Find("P3Cam");
 				playerCamSet = playerCam.GetComponent<Camera>();
 				playerCamSet.rect = new Rect(0 , 0, 1, 0.5f);
@@ -69,25 +67,25 @@ public class scriptGameManager : MonoBehaviour {
 			else if(playerNum == 4)
 			{
 				playerActivate = GameObject.Find("Player1");
-				player1Spawn = playerActivate.transform.position;
+
 				playerCam = GameObject.Find("P1Cam");
 				playerCamSet = playerCam.GetComponent<Camera>();
 				playerCamSet.rect = new Rect(0, 0.5f, 0.5f, 0.5f);
 
 				playerActivate = GameObject.Find("Player2");
-				player2Spawn = playerActivate.transform.position;
+
 				playerCam = GameObject.Find("P2Cam");
 				playerCamSet = playerCam.GetComponent<Camera>();
 				playerCamSet.rect = new Rect(0.5f , 0.5f, 0.5f, 0.5f);
 
 				playerActivate = GameObject.Find("Player3");
-				player3Spawn = playerActivate.transform.position;
+
 				playerCam = GameObject.Find("P3Cam");
 				playerCamSet = playerCam.GetComponent<Camera>();
 				playerCamSet.rect = new Rect(0 , 0, 0.5f, 0.5f);
 
 				playerActivate = GameObject.Find("Player4");
-				player4Spawn = playerActivate.transform.position;
+
 				playerCam = GameObject.Find("P4Cam");
 				playerCamSet = playerCam.GetComponent<Camera>();
 				playerCamSet.rect = new Rect(0.5f , 0, 0.5f, 0.5f);
@@ -95,6 +93,7 @@ public class scriptGameManager : MonoBehaviour {
 			}
 		
 			gameStart = true;
+			print (players.Length);
 		}
 	}
 
@@ -107,10 +106,16 @@ public class scriptGameManager : MonoBehaviour {
 	{
 		deadPlayers++;
 
-		if(deadPlayers == 3)
+		if(deadPlayers == playerNum - 1)
 		{
-			Application.LoadLevel("Main Menu");
+			StartCoroutine("BackToMainMenu");
 		}
 
+	}
+
+	private IEnumerator BackToMainMenu()
+	{
+		yield return new WaitForSeconds(5f);
+		Application.LoadLevel("Main Menu");
 	}
 }
