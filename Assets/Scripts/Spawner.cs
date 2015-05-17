dbb63bf91b32df69b53ece9812fdacc48406d45a
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2015-05-16 23:30:24 -0400

    tweaks

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,9 +7,15 @@ public class Spawner : MonoBehaviour{
 
 	void Start(){
 		rotations = new Quaternion[4]{new Quaternion(0,0,0,0),new Quaternion(0,90,0,0),new Quaternion(0,180,0,0),new Quaternion(0,270,0,0)};
-		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,0,0),rotations[Random.Range(0,rotations.Length-1)]);
-		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,0,24),rotations[Random.Range(0,rotations.Length-1)]);
-		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,0,0),rotations[Random.Range(0,rotations.Length-1)]);
-		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,0,24),rotations[Random.Range(0,rotations.Length-1)]);
+
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,0,0),rotations[Random.Range(0,rotations.Length)]);
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,0,24),rotations[Random.Range(0,rotations.Length)]);
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,0,0),rotations[Random.Range(0,rotations.Length)]);
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,0,24),rotations[Random.Range(0,rotations.Length)]);
+
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,8.44f,0),new Quaternion(180,rotations[Random.Range(0,rotations.Length)].y,0,0));
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(0,8.44f,24),new Quaternion(180,rotations[Random.Range(0,rotations.Length)].y,0,0));
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,8.44f,0),new Quaternion(180,rotations[Random.Range(0,rotations.Length)].y,0,0));
+		Instantiate(layouts[Random.Range(0,layouts.Length)],new Vector3(-24,8.44f,24),new Quaternion(180,rotations[Random.Range(0,rotations.Length)].y,0,0));
 	}
 }
